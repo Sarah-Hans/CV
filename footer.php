28f49fe0d8d3ce689a636a7d8a19945560907c2e
Sarah <leawine@gmail.com>
2021-01-11 11:20:30 +0100

    Changement des chemins pour appeler style et images

--- footer.php
+++ footer.php
@@ -9,8 +9,8 @@
     <div id="reseaux">
         <p>Suivez-moi :</p>
         <div id="img-reseaux">
-            <a href="https://fr.linkedin.com/in/sarahans"><img src="images/icone-linkedin.png" alt="Profil LinkedIn" title="Profil Twitter"></a>
-            <a href="https://twitter.com/lageekette"><img src="images/icone-twitter.png" alt="Profil Twitter" title="Profil LinkedIn"></a>
+            <a href="https://fr.linkedin.com/in/sarahans"><img src="../images/icone-linkedin.png" alt="Profil LinkedIn" title="Profil Twitter"></a>
+            <a href="https://twitter.com/lageekette"><img src="../images/icone-twitter.png" alt="Profil Twitter" title="Profil LinkedIn"></a>
         </div>
         <p id="majcv">CV mis à jours le 07/01/2021</p>
     </div>
--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="styles/style.css" rel="stylesheet" type="text/css">
+    <link href="../styles/style.css" rel="stylesheet" type="text/css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&family=Dancing+Script&family=Overlock&display=swap" rel="stylesheet">
     <title><?=$metaTitle?></title>
@@ -20,7 +20,7 @@
         </a>
     </div>
     <div id="header-profilpicture">
-        <img src="images/sarah-hans.jpg" alt="photo de profil" title="Photo de Sarah Hans">
+        <img src="../images/sarah-hans.jpg" alt="photo de profil" title="Photo de Sarah Hans">
     </div>
 </header>
 <nav class="nav">
