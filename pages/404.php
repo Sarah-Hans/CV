c25103f73d9cb0dd8351fde1d137b6fcb7d1e3ee
Sarah <leawine@gmail.com>
2021-01-11 15:29:39 +0100

    ajout d'une page d'erreur 404

---
+++ pages/404.php
@@ -0,0 +1,11 @@
+<?php
+require 'header.php';
+
+echo 'Erreur 404 - la page que vous cherchez est introuvable.';
+echo '<br>';
+echo '<a href="/">Revenez en page accueil</a>';
+
+require 'footer.php';
+
+
+
