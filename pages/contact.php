6e04d7f234d26f19782d051ef3aa0a0b86ab0d7a
Sarah <leawine@gmail.com>
2021-01-11 09:47:44 +0100

    déplacement des 3 pages de contenus dans le dossier pages

---
+++ pages/contact.php
@@ -0,0 +1,71 @@
+<?php
+$metaTitle = "Contacter Sarah Hans";
+$metaDescription ="N'hésitez pas à me contacter pour avoir davantage d'informations sur mon profil, mon parcours et mes compétences.";
+?>
+
+<?php
+require 'header.php';
+?>
+
+<main>
+            <h1>Contactez-moi</h1>
+            <div id="contact">
+                <section id="formulaire">
+                    <h2>Formulaire de contact</h2>
+                    <p>Si vous souhaitez me contacter, n'hésitez pas à m'envoyer un message en utilisant le formulaire ci-dessous.</p>
+                    <form action="https://httpbin.org/post" method="post">
+                        <div id="genre">
+                            <input type="radio" id="madame" name="genre" value="madame">
+                            <label for="madame">Mme</label>
+                            <input type="radio" id="monsieur" name="genre" value="monsieur">
+                            <label for="madame">Mr</label>
+                        </div>
+                        <div id="nom-prenom" class="form-item">
+                            <div id="nom">
+                                <label for="name">Nom<em>*</em> :</label>
+                                <input type="text" id="name" name="user_name" placeholder="Votre nom" required>
+                            </div>
+                            <div id="prenom">
+                                <label for="firstname">Prénom<em>*</em> :</label>
+                                <input type="text" id="firstname" name="user_firstname" placeholder="Votre prénom" required>
+                            </div>
+                        </div>
+                        <div id="champs-email" class="form-item">
+                            <label for="mail">Adresse e-mail<em>*</em> :</label>
+                            <input type="email" id="mail" name="user-mail" placeholder="Votre e-mail" required>
+                        </div>
+                        <div id="liste-choix" class="form-item">
+                            <label for="choix">Motif de contact :</label>
+                            <select id="choix">
+                                <option value="">-----Sélectionnez une option-----</option>
+                                <option value="contact">Prise de contact</option>
+                                <option value="proposition-poste">Proposition de poste</option>
+                                <option value="renseignements">Besoin de renseignements</option>
+                            </select>
+                        </div>
+                        <div id="champs-msg">
+                            <label for="msg">Votre message :</label>
+                            <textarea id="msg" name="user-msg" rows="5" placeholder="Votre message ici"></textarea>
+                        </div>
+                        <div class="bouton">
+                            <button type="submit">Envoyer</button>
+                        </div>
+                    </form>
+                </section>
+
+                <section id="infospratiques">
+                    <h2>Infos pratiques</h2>
+                    <p><strong>Adresse : </strong><br>
+                    70 chemin du Château - 26320 Saint Marcel Les Valence</p>
+                    <p><strong>Téléphone :</strong> 06 59 96 65 84</p>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d176.4230377665115!2d4.933132401128772!3d44.96869527848632!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDTCsDU4JzA3LjQiTiA0wrA1NSc1OS43IkU!5e0!3m2!1sfr!2sfr!4v1607092285228!5m2!1sfr!2sfr" width="600" height="450" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                </section>
+
+            </div>
+            
+
+    </main>
+
+<?php
+include 'footer.php';
+?>
---
+++ pages/index.php
@@ -0,0 +1,155 @@
+<?php
+$metaTitle = "CV de Sarah Hans";
+$metaDescription ="Spécialisée en webmarketing et développement, découvrez mes compétences et mon expérience professionnelle";
+?>
+
+<?php
+require 'header.php';
+?>
+
+<main>
+        <section id="intro">
+            <h1>CV Sarah Hans - Développement web et Webmarketing</h1>
+            <h2>Informations générales</h2>
+            <ul id="motcles">
+                <li class="motcles-item">SEO</li>
+                <li class="motcles-item">Développement web</li>
+                <li class="motcles-item">Rédaction Web</li>
+                <li class="motcles-item">SEA</li>
+                <li class="motcles-item">Community Management</li>
+            </ul>
+            <p id="infosgen"><strong>34 ans - Saint-Marcel-Les-Valence (FR) - Permis B - Véhicule personnel</strong></p>
+            <p>Bienvenue sur mon CV en ligne. <br>
+            Je suis à la recherche de <strong>nouvelles opportunités professionnelles.</strong><br>
+            Vous trouverez sur ce site mes domaines de compétences ainsi que le détail de mes expériences professionnelles passées et de mes formations. Sur la page <span id="intro-hobbyelevage"><a href="hobby-elevage.html">Hobby Principal</a></span>, vous pourrez avoir plus d'information sur mon activité accessoire d'élevage de chats de race. </p>
+        </section>
+        <div id="leftside">
+            <section id="skills">
+                <h2>Mes compétences</h2>
+                <h3>Informatique</h3>
+                <ul>
+                    <li>Systèmes d'exploitation : MAC OS, Windows</li>
+                    <li>Bureautique : Suite Microsoft Office</li>
+                    <li>Graphisme : Photoshop, Lightroom</li>
+                    <li>Vidéo : </li>
+                    <li>CMS : Wordpress, Prestashop, Magento</li>
+                </ul>
+                <h3>Webmarketing</h3>
+                <ul>
+                    <li>SEO : maitrise des techniques de référencement naturel</li>
+                    <li>SEA : Adwords, Facebook Ads, Adsense, Affiliation</li>
+                    <li>SMO : maîtrise des réseaux sociaux (Instagram, Facebook, Twitter, Pinterest, Youtube)</li>
+                    <li>Rédaction web</li>
+                    <li>Création et envoi d'e-mailings/newsletters</li>
+                    <li>...</li>
+                </ul>
+                <h3>Développement web</h3>
+                <ul>
+                    <li>HTML / CSS</li>
+                    <li>Utilisation de GIT</li>
+                    <li>...</li>
+                </ul>
+            </section>
+            <section id="hobbys">
+                <h2>Mes hobbys</h2>
+                <ol>
+                    <li>Élevage de chats</li>
+                    <li>Photographie</li>
+                    <li>Randonnée</li>
+                    <li>Vélo</li>
+                    <li>Blogging / réseaux sociaux</li>
+                </ol>
+            </section>
+        </div>
+        <div id="rightside">
+            <section id="xp">
+                <h2>Expériences professionnelles</h2>
+                <p>Ci-dessous, voici un récapitulatif de mes expériences professionnelles.</p>
+                <table>
+                    <tr class="xp-1eligne">
+                        <td>Période</td>
+                        <td>Poste</td>
+                        <td>Entreprise</td>
+                        <td>Missions</td>
+                    </tr>
+                    <tr>
+                        <td>05/2018 - 05/2020</td>
+                        <td>Community & Product manager</td>
+                        <td>Mon-Cheval</td>
+                        <td>community management, SEO, SEA, rédaction web, e-mailing, web analytics</td>
+                    </tr>
+                    <tr>
+                        <td>12/2017 - 04/2018</td>
+                        <td>Chargée de webmarketing</td>
+                        <td>26 Diffusions</td>
+                        <td>community management, SEO, SEA, rédaction web, e-mailing, web analytics</td>
+                    </tr>
+                    <tr>
+                        <td>04/2017 - 12/2017</td>
+                        <td>Community manager et SEO</td>
+                        <td>Vivandis</td>
+                        <td>community management, SEO, SEA, rédaction web, e-mailing, web analytics</td>
+                    </tr>
+                    <tr>
+                        <td>06/2015 - 04/2017</td>
+                        <td>Community & product manager</td>
+                        <td>Mon-Cheval</td>
+                        <td>community management, SEO, SEA, rédaction web, e-mailing, web analytics</td>
+                    </tr>
+                    <tr>
+                        <td>09/2014 - 04/2015</td>
+                        <td>Webmarketer</td>
+                        <td>Valrhona</td>
+                        <td>SEO, SEA, Réseaux sociaux, rédaction, stratégie d'acquisition</td>
+                    </tr>
+                    <tr>
+                        <td>01/2013 - 12/2013</td>
+                        <td>Responsable webmarketing</td>
+                        <td>Too Easy</td>
+                        <td>Stratégie web globale, Adwords, Social media management, rédaction web, formations, web analytics, SEO</td>
+                    </tr>
+                </table>
+            </section>
+            <section id="formations">
+                <h2>Formations</h2>
+                <p>Ci-dessous, voici un récapitulatif de mes formations.</p>
+                <table>
+                    <tr class="xp-1eligne">
+                        <td>Période</td>
+                        <td>Diplôme</td>
+                        <td>Ecole</td>
+                        <td>Détails</td>
+                    </tr>
+                    <tr>
+                        <td>2020-en cours</td>
+                        <td>Technicien Développeur</td>
+                        <td>Campus Numérique in The Alps</td>
+                        <td>HTML/CSS, PHP, Javascript, Java, Réseaux...</td>
+                    </tr>
+                    <tr>
+                        <td>2010-2011</td>
+                        <td>Master 2 IDEMM</td>
+                        <td>Université Lille 3</td>
+                        <td>Ingénierie documentaire, édition et médiation multimédia</td>
+                    </tr>
+                    <tr>
+                        <td>2009 -2010</td>
+                        <td>Master 1 ICD</td>
+                        <td>Université Lille 3</td>
+                        <td>Information, communication et document</td>
+                    </tr>
+                    <tr>
+                        <td>2005-2009</td>
+                        <td>Licence en Information et Communication</td>
+                        <td>Université Libre de Bruxelles</td>
+                        <td>Journalisme et communication avec option informatique</td>
+                    </tr>
+                </table>
+            </section>
+        </div>
+
+    </main>
+
+<?php
+include 'footer.php';
+?>
