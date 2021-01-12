de1d630359cdaf5fdabc0a364fd1f0cd4715fd0e
Sarah <leawine@gmail.com>
2021-01-12 09:45:05 +0100

    Ajout des meta à la page 404

--- pages/404.php
+++ pages/404.php
@@ -1,4 +1,7 @@
 <?php
+$metaTitle = "Page introuvable";
+$metaDescription = "La page que vous recherchez n'existe pas.";
+
 require 'header.php';
 
 echo 'Erreur 404 - la page que vous cherchez est introuvable.';
