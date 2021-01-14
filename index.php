ef54ac66cb393750d36eb3769ec4dd3347e6db15
Sarah <leawine@gmail.com>
2021-01-14 16:34:09 +0100

    ajout d'un tableau des routes pour associer les pages

--- index.php
+++ index.php
@@ -2,16 +2,19 @@
 
 $page = filter_input(INPUT_GET, 'page', FILTER_SANITIZE_URL);
 
-if(isset($page)) {
-    if($page == 'contact') {
-        require 'pages/contact.php';
-    }
-    else if($page == 'hobbyelevage') {
-        require 'pages/hobby-elevage.php';
-    }
-    else {
-        require 'pages/404.php';
+$routes = [
+    'contact' => 'pages/contact.php',
+    'hobbyelevage' => 'pages/hobby-elevage.php',
+    '404' => 'pages/404.php',
+    'accueil' => 'pages/accueil.php'
+];
+
+if (isset($page)) {
+    if (array_key_exists($page, $routes)) {
+        require $routes[$page];
+    } else {
+        require $routes['404'];
     }
 } else {
-    require 'pages/accueil.php';
+    require $routes['accueil'];
 }
